ddc1e00a89a610425ee9da6c1344a3c40e4b5585
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-05-05 18:54:32 +0530

    Update ActiveFriend.jsx

--- messenger/frontend/src/components/ActiveFriend.jsx
+++ messenger/frontend/src/components/ActiveFriend.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+const BACKEND_URL = "https://mern-chat-application-nlxu.onrender.com";
 const ActiveFriend = ({ user, setCurrentFriend }) => {
   return (
     <div
@@ -16,15 +16,18 @@ const ActiveFriend = ({ user, setCurrentFriend }) => {
       <div className="image-active-icon">
         <div className="image">
           <img 
-            src={`/image/${user.userInfo.image}`} 
+            src={`${BACKEND_URL}/image/${user.userInfo.image}`} 
             alt="" 
             onError={(e) => {
               e.target.onerror = null;
-              e.target.src = '/image/default-profile-picture1.png';
+              e.target.src = `${BACKEND_URL}/image/default-profile-picture1.png`;
             }}
           />
           <div className="active-icon"></div>
         </div>
+      </div>
+      <div className="user-name">
+        {user.userInfo.userName}
       </div>
     </div>
   );
