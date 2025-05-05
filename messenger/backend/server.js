a5f60e0c7c9123881398ae0b59f3298206714883
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-05-05 20:52:55 +0530

    Update server.js

--- messenger/backend/server.js
+++ messenger/backend/server.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://localhost:8000",
   "http://localhost:5000",
-  "http://127.0.0.1:8000"
+  "http://127.0.0.1:8000",
+  "https://mern-chat-hk3u.onrender.com"
 ];
 
 // --- Middlewares ---
