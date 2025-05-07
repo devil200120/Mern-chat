0979325bb1b013169645f172e06f94964aef03ea
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-05-07 19:05:16 +0530

    Update GroupInfo.jsx

--- messenger/frontend/src/components/GroupInfo.jsx
+++ messenger/frontend/src/components/GroupInfo.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { FaCaretSquareDown } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 
-const BACKEND_URL = process.env.REACT_APP_API_URL || "https://mern-chat-hk3u.onrender.com";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://mern-chat-application-nlxu.onrender.com';
+
 
 const GroupInfo = ({ currentGroup }) => {
   const { friends } = useSelector(state => state.messenger);
