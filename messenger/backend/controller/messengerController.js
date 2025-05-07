60f6cab4a2ea836452ee91d2cdf73c2bf9ed56c7
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-05-07 18:30:26 +0530

    Update messengerController.js

--- messenger/backend/controller/messengerController.js
+++ messenger/backend/controller/messengerController.js
@@ -158,7 +158,7 @@ module.exports.ImageMessageSend = (req,res) => {
               imageName 
           } = fields;
 
-          const newPath = path.join(__dirname, '../uploads/', imageName)
+          const newPath = __dirname + `../../../frontend/public/image/${imageName}`
           files.image.originalFilename = imageName;
 
           try{
