7d88a13a86d8326bbc7c7366c303d625bc94e633
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-05-07 19:06:37 +0530

    Update Message.jsx

--- messenger/frontend/src/components/Message.jsx
+++ messenger/frontend/src/components/Message.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { FaRegCheckCircle } from "react-icons/fa";
 
-const BACKEND_URL = process.env.REACT_APP_API_URL || "https://mern-chat-hk3u.onrender.com";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://mern-chat-application-nlxu.onrender.com';
+
 const DEFAULT_AVATAR = `${BACKEND_URL}/uploads/default-profile-picture1.png`;
 
 const Message = ({ message, currentfriend, scrollRef, typingMessage }) => {
